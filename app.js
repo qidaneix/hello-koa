f3aa8446ad5b5d67accf9fce680da4aa301d4635
肖金 <xiaojin1992@126.com>
2017-12-12 00:47:29 +0800

    。。

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
 const Koa = require('koa');
 
-// 创建一个Koa对象表示web app本身:
+const bodyParser = require('koa-bodyparser');
+
+const controller = require('./controller');
+
 const app = new Koa();
 
-// 对于任何请求，app将调用该异步函数处理请求：
+// log request URL:
 app.use(async (ctx, next) => {
+    console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
     await next();
-    // 设置response的Content-Type:
-    ctx.response.type = 'text/html';
-    // 设置response的内容:
-    ctx.response.body = '<h1>Hello, koa2!</h1>';
 });
 
-// 在端口80监听:
-app.listen(80);
-console.log('app started at port 80...');
+// parse request body:
+app.use(bodyParser());
+
+// add controllers:
+app.use(controller());
+
+app.listen(3000);
+console.log('app started at port 3000...');
